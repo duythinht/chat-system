8271f72cd6b837887d502e1345d2e3eb486c660e
Thinh Tran <duythinht@gmail.com>
2016-01-13 10:36:09 +0700

    Refactor to es6 string template

--- endpoint_provider/server.js
+++ endpoint_provider/server.js
@@ -2,14 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var redis = require('redis');
 var bodyParser = require('body-parser');
-var util = require('util');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/users/:userId/messages', (req, res) => {
   var redisClient = redis.createClient();
-  var channel = util.format('chat:user:%s', req.params.userId);
+  var channel = `chat:user:${req.params.userId`;
   redisClient.publish(channel, JSON.stringify(req.body));
   res.send({status: 'ok'});
 });
